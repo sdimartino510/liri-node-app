6032749a7e11f3a07cbfddf37fef4eabcfee2d74
ArchangelD <sdimartino@gmail.com>
2019-02-23 14:05:37 -0800

    Adds bandsintown functionality

--- liri.js
+++ liri.js
@@ -9,18 +9,18 @@ var spotify = new Spotify(keys.spotify);
 
 var spotifyThisSong = function (songName) {
     if (songName === "") {
-        songName = "What's My Age Again";
+        songName = "The Sign";
     }
     spotify
         .search({ type: 'track', query: songName })
         .then(function (response) {
-            for (var i = 0; i < response.tracks.items.length; i++) {
-                console.log(response.tracks.items[i].artists[0].name);
-                console.log(response.tracks.items[i].name);
-                console.log(response.tracks.items[i].preview_url);
-                console.log(response.tracks.items[i].album.name);
-                console.log("---------------------");
-            }
+            // for (var i = 0; i < response.tracks.items.length; i++) {
+                console.log(response.tracks.items[0].artists[0].name);
+                console.log(response.tracks.items[0].name);
+                console.log(response.tracks.items[0].preview_url);
+                console.log(response.tracks.items[0].album.name);
+                console.log("--------------------------------------");
+            // }
             
         })
         .catch(function (err) {
@@ -30,6 +30,21 @@ var spotifyThisSong = function (songName) {
 
 var concertThis = function (bandName) {
 
+    axios.get("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            console.log("-----------------------------");
+            console.log(response.data[0].venue.name);
+            console.log(response.data[0].venue.city);
+            console.log(response.data[0].venue.region);
+            console.log(response.data[0].venue.country);
+            console.log(moment(response.data[0].datetime).format("MM-DD-YYYY"));
+            console.log("-----------------------------");
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+
+
 }
 
 var movieThis = function (movieName) {
@@ -38,6 +53,7 @@ var movieThis = function (movieName) {
     }
     axios.get('http://omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json&apikey=trilogy')
         .then(function (response) {
+            // console.log(response.data);
             console.log(response.data.Title);
             console.log(response.data.Year);
             console.log(response.data.Rated);
