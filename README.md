4d252ecdcd5cdf170b55a37444eaf3dab0e3dfa1
ArchangelD <sdimartino@gmail.com>
2019-02-24 21:44:56 -0800

    Adds do-what-it-says functionality, adds response formatting, cleans up code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# liri-node-app
+## liri-node-app
 Unit 10 Homework
+
+LIRI
+====
+
+LIRI stands for "Language Interpretation and Recognition Interface." LIRI is a command-line node app that takes in a command and a subject for the command, and returns the relevant data.
+
+## How to use LIRI:
+Type a command a command and a subject for the command into the command-line.
+
+Commands:
+* spotify-this-song: When followed by a song title, this command will return the name of the artist, the song title, a preview link, and the name of the album the song is from.
+* concert-this: When followed by a band or artist, this command will return information for that band or artist's next performance - name of the venue, city, region/state, and country followed by the date of the show.
+* movie-this: When followed by the title of a movie, this command will return the movie title, the year of release, the MPAA rating, the IMDB rating, the Rotten Tomatoes rating, the country where the movie was produced, the language of the movie, the plot, and the actors in the film.
--- liri.js
+++ liri.js
@@ -6,75 +6,88 @@ var moment = require("moment");
 var fs = require("fs");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-// var random = require("./random.txt");
 
 var spotifyThisSong = function (songName) {
+
     if (songName === "") {
-        songName = "The Sign";
+        songName = "The Sign Ace of Base";
     }
+
     spotify
         .search({ type: 'track', query: songName })
         .then(function (response) {
-            // for (var i = 0; i < response.tracks.items.length; i++) {
-                // console.log(response);
-                console.log(response.tracks.items[0].artists[0].name);
-                console.log(response.tracks.items[0].name);
-                console.log(response.tracks.items[0].preview_url);
-                console.log(response.tracks.items[0].album.name);
-                console.log("--------------------------------------");
-            // }
-            
+
+                console.log("-------------------------------------------------------");
+                console.log("Artist: " + response.tracks.items[0].artists[0].name);
+                console.log("Song Title: " + response.tracks.items[0].name);
+                console.log("Album Name: " + response.tracks.items[0].album.name);
+                console.log("Preview URL: " + response.tracks.items[0].preview_url);
+                console.log("-------------------------------------------------------");
+
         })
+
         .catch(function (err) {
             console.log(err);
+
         });
+
 }
 
 var concertThis = function (bandName) {
 
     axios.get("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp")
         .then(function (response) {
-            console.log("-----------------------------");
-            console.log(response.data[0].venue.name);
-            console.log(response.data[0].venue.city);
-            console.log(response.data[0].venue.region);
-            console.log(response.data[0].venue.country);
-            console.log(moment(response.data[0].datetime).format("MM-DD-YYYY"));
-            console.log("-----------------------------");
-        })
-        .catch(function (err) {
-            console.log(err);
+            console.log("-------------------------------------------------");
+            console.log("Venue/Event Name: " + response.data[0].venue.name);
+            console.log("City: " + response.data[0].venue.city);
+            console.log("State/Region: " + response.data[0].venue.region);
+            console.log("Country: " + response.data[0].venue.country);
+            console.log("Date: " + moment(response.data[0].datetime).format("MM-DD-YYYY"));
+            console.log("-------------------------------------------------");
+
         })
 
+        .catch(function (err) {
+            console.log(err);
+
+        })
 
 }
 
 var movieThis = function (movieName) {
+
     if (movieName === "") {
         movieName = "Mr. Nobody";
     }
+
     axios.get('http://omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json&apikey=trilogy')
         .then(function (response) {
-            // console.log(response.data);
-            console.log(response.data.Title);
-            console.log(response.data.Year);
-            console.log(response.data.Rated);
-            console.log(response.data.imdbRating);
-            console.log(response.data.Country);
-            console.log(response.data.Language);
-            console.log(response.data.Plot);
-            console.log(response.data.Actors);
-            console.log(response.data.Ratings[1].Value);
-            console.log("-----------------");
+
+            console.log("------------------------------------");
+            console.log("Movie Title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("MPAA Rating: " + response.data.Rated);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Production Country: " + response.data.Country);
+            console.log("Language of Movie: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Cast: " + response.data.Actors);
+            console.log("------------------------------------");
+
         })
+
         .catch(function (error) {
             console.log(error);
+
         });
+
 }
 
 var doWhatItSays = function () {
 
     fs.readFile("random.txt", "utf8", function(err, data) {
+
         if (err) {
             return console.log(err);
         }
@@ -83,11 +96,13 @@ var doWhatItSays = function () {
         userChoice = data[1];
         runCommand(userCommand, userChoice);
         console.log(data);
+
     })
 
 }
 
 var runCommand = function (userCommand, userChoice) {
+
     switch (userCommand) {
         case "spotify-this-song":
             spotifyThisSong(userChoice);
@@ -103,7 +118,9 @@ var runCommand = function (userCommand, userChoice) {
             break;
         default:
             console.log("LIRI does not recognize that command!");
+
     }
+    
 }
 
 var executeCommand = function (arg1, arg2) {
